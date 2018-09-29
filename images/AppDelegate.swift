a5224bb2ee7c2e0e185621d6e003766f0e8eab9f
Koray Birand <info@koraybirand.com>
2018-09-30 02:50:20 +0300

    Initial Commit

---
+++ images/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  images
+//
+//  Created by Koray Birand on 9/30/18.
+//  Copyright © 2018 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ images/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  images
+//
+//  Created by Koray Birand on 9/30/18.
+//  Copyright © 2018 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
